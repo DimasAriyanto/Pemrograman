8b3af59b7565a227aa13ac8de67199bcff35d2c3
DimasAriyanto <dimas.ariyanto32@gmail.com>
2022-04-20 09:39:15 +0700

    Memasukan codingan responsi

---
+++ ResponsiPemrograman4408/ResponsiPemrograman4408/Karyawan.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4408
+{
+    internal class Karyawan
+    {
+        private int NIK { get; set; }
+        private string Nama { get; set; }
+        private float GajiBulanan { get; set; }
+
+        public Karyawan(int nik, string nama, float gaji)
+        {
+            this.NIK = nik;
+            this.Nama = nama;
+            this.GajiBulanan = gaji;
+        }
+
+        public int getNik()
+        {
+            return NIK;
+        }
+
+        public string getNama()
+        {
+            return Nama;
+        }
+
+        public float getGajiBulanan()
+        {
+            if (GajiBulanan < 0)
+            {
+                GajiBulanan = 0;
+            }
+
+            return GajiBulanan;
+        }
+
+        public void NaikkanGaji(float persentase)
+        {
+            this.GajiBulanan = persentase / 100 * this.GajiBulanan + this.GajiBulanan;
+        }
+
+    }
+}
---
+++ ResponsiPemrograman4408/ResponsiPemrograman4408/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4408
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan paijo = new Karyawan(190302123, "Paijo", 3000000);
+            Karyawan jono = new Karyawan(190302124, "Jono", 2000000);
+            Console.WriteLine("No   Nik/Nama        Gaji Bulanan");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("1    {0}/{1}         {2}", paijo.getNik(), paijo.getNama(), paijo.getGajiBulanan());
+            Console.WriteLine("2    {0}/{1}         {2}", jono.getNik(), jono.getNama(), jono.getGajiBulanan());
+
+
+            Console.WriteLine();
+            Console.WriteLine("Naikkan Gaji Sebesar 10%");
+            Console.WriteLine();
+
+            paijo.NaikkanGaji(10);
+            jono.NaikkanGaji(10);
+
+            Console.WriteLine("No   Nik/Nama        Gaji Bulanan");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("1    {0}/{1}         {2}", paijo.getNik(), paijo.getNama(), paijo.getGajiBulanan());
+            Console.WriteLine("2    {0}/{1}         {2}", jono.getNik(), jono.getNama(), jono.getGajiBulanan());
+
+            Console.ReadKey();
+        }
+    }
+}
