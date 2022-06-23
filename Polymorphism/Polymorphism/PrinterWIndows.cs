318b82a80c53339e2350e3bae6778f76b9db5668
DimasAriyanto <dimas.ariyanto32@gmail.com>
2022-06-24 02:14:51 +0700

    Input cosingan PrinterWindows

---
+++ Polymorphism/Polymorphism/PrinterWIndows.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class PrinterWIndows
+    {
+        public virtual void Show()
+        { }
+
+        public virtual void Print()
+        { }
+    }
+}
