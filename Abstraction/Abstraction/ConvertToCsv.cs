55487370561cf35df2247f6acf40d21167dcc4c3
DimasAriyanto <dimas.ariyanto32@gmail.com>
2022-07-16 09:46:31 +0700

    Reponsi UAS Pemrograman

---
+++ Abstraction/Abstraction/ConvertToCsv.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    internal class ConvertToCsv : ConvertObject
+    {
+        public override void Convert(Mahasiswa mhs)
+        {
+            string csv = string.Format("{0}, {1}, {2}", mhs.npm, mhs.nama, mhs.email);
+
+            Console.WriteLine("Hasil konversi ke csv : \n");
+            Console.WriteLine(csv);
+        }
+    }
+}
